1a41a4ba3289732c729201748cdb22decf616af4
Van <van-rottana.you@hotmail.com>
2018-10-25 14:19:09 +0200

    suppresion import en trop

--- ElPatatas/src/app/comptabilite/comptabilite.module.ts
+++ ElPatatas/src/app/comptabilite/comptabilite.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComposentComponent } from './composent/composent.component';
 import { ComponentsComponent } from './components/components.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ComposentComponent, ComponentsComponent]
+  declarations: [ ComponentsComponent]
 })
 export class ComptabiliteModule { }
