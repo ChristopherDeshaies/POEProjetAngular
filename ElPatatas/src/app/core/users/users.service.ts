3e7316b1d1c0fc79e63d3a2018cc705d3e6ea656
van-rottana YOU <van-rottana.you@hotmail.com>
2018-10-28 21:26:43 +0100

    ajout CRUD utilisateurs

--- ElPatatas/src/app/core/users/users.service.ts
+++ ElPatatas/src/app/core/users/users.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { map, delay, catchError, finalize, tap } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { User } from './models/user';
+import { Router } from '@angular/router';
 
 const urlUser = "http://127.0.0.1:3000/users";
 
@@ -11,11 +11,51 @@ const urlUser = "http://127.0.0.1:3000/users";
 })
 export class UsersService {
 
-  user: User;
-  
   constructor(
-    private httpClient: HttpClient,
+    private httpclient: HttpClient,
+    private router: Router
   ) { }
+
+  getListUsers(): Observable<User[]> {
+    return this.httpclient.get<User[]>(urlUser);
+  }
+
+  putUser(user: User) {
+    const url = `${urlUser}/${user.getId()}`;
+    this.httpclient.put(url,
+      user)
+      .subscribe(
+        data => {
+          console.log("PUT Request is successful ", data);
+        },
+        error => {
+          console.log("Modification de l'utilisateur", error);
+        }
+      );
+  }
+
+
+  postUser(user: User) {
+    this.httpclient.post(urlUser,
+      user)
+      .subscribe(
+        data => {
+          console.log("POST Request is successful ", data);
+        },
+        error => {
+          console.log("Ajout de l'utilisateur", error);
+        }
+      );
+  }
+
+  deleteUser(id: number): void {
+    const url = `${urlUser}/${id}`;
+    this.httpclient.delete(url)
+      .subscribe(error => {
+                            console.log("Suppression de l'utilisateur", error);
+                          }
+      );
+  }
 
   
 }
