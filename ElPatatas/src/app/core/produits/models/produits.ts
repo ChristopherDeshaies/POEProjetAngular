81bcc16e23e18397572e3797a0d3e5ece649e311
Van <van-rottana.you@hotmail.com>
2018-10-30 15:44:24 +0100

    Changement produits

--- ElPatatas/src/app/core/produits/models/produits.ts
+++ ElPatatas/src/app/core/produits/models/produits.ts
@@ -8,27 +8,37 @@ export class Produits {
     dateAchat: string;
     prixAchat: number;
 
-    constructor(
-        id: number,
-        libelle: string,
-        codeFournisseur: number,
-        quantiteInitiale: number,
-        quantiteRestante: number,
-        dateLimite: string,
-        dateAchat: string,
-        prixAchat: number,
-    ){}
-
-    public static fromJson(json: Object): Produits {
-        return new Produits(
-            json['id'],
-            json['libelle'],
-            json['codeFournisseur'],
-            json['quantiteInitiale'],
-            json['quantiteRestante'],
-            json['dateLimite'],
-            json['dateAchat'],
-            json['prixAchat'],
-        );
+    constructor (libelle, codeFournisseur, quantiteInitiale, dateLimite, dateAchat, prixAchat){
+        this.libelle=libelle;
+        this.codeFournisseur=codeFournisseur;
+        this.quantiteInitiale=quantiteInitiale;
+        this.quantiteRestante=quantiteInitiale;
+        this.dateLimite=dateLimite;
+        this.dateAchat=dateAchat;
+        this.prixAchat=prixAchat;
     }
+    
+    // constructor(
+    //     id: number,
+    //     libelle: string,
+    //     codeFournisseur: number,
+    //     quantiteInitiale: number,
+    //     quantiteRestante: number,
+    //     dateLimite: string,
+    //     dateAchat: string,
+    //     prixAchat: number,
+    // ){}
+
+    // public static fromJson(json: Object): Produits {
+    //     return new Produits(
+    //         json['id'],
+    //         json['libelle'],
+    //         json['codeFournisseur'],
+    //         json['quantiteInitiale'],
+    //         json['quantiteRestante'],
+    //         json['dateLimite'],
+    //         json['dateAchat'],
+    //         json['prixAchat'],
+    //     );
+    // }
 }
