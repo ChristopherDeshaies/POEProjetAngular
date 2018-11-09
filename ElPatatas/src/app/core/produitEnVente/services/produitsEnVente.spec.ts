102e239c7b916e62f837c5249c569fefc4f5e2bc
Formation <llyodoisdu59@hotmail.fr>
2018-11-09 14:44:00 +0100

    conflict db.json

--- ElPatatas/src/app/core/produitEnVente/services/produitsEnVente.spec.ts
+++ ElPatatas/src/app/core/produitEnVente/services/produitsEnVente.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ProduitsEnVenteService } from './produitsEnVente';
 
-describe('StocksService', () => {
+describe('ProduitsEnVenteService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
